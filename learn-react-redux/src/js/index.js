2fd107ed5089f2f93e635c312f7e3515306fab59
hobk1ard <hobbes.kinard@gmail.com>
2018-10-03 17:11:14 -0500

    Display List

--- learn-react-redux/src/js/index.js
+++ learn-react-redux/src/js/index.js
@@ -1,8 +1,16 @@
-import store from "../js/store/store";
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import App from "./components/App";
 import { addArticle } from "../js/actions/actions";
 
-window.store = store;
-window.addArticle = addArticle;
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-store.subscribe(() => console.log('Look ma, Redux!!'))
-store.dispatch( addArticle({ name: 'React Redux Tutorial for Beginners', id: 1 }) )
+store.subscribe(() => console.log('Look ma, Redux!!'));
+store.dispatch( addArticle({ title: 'React Redux Tutorial for Beginners', id: 1 }) );
